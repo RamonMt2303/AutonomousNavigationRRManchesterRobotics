5161562c17d2970e5d5064fa0ae2dce04f42f5e6
RamonMt2303 <A01702917@itesm.mx>
2024-05-29 08:16:24 -0600

    Launch file añadido

--- challenge_final/scripts/aruco_detect.py
+++ challenge_final/scripts/aruco_detect.py
@@ -2,6 +2,7 @@
 
 from fiducial_msgs.msg import FiducialTransformArray
 from std_msgs.msg import Float32MultiArray
+from geometry_msgs.msg import Pose
 from tf.transformations import euler_from_quaternion
 import rospy
 import tf2_ros
@@ -15,7 +16,7 @@ class ArucoDetect():
         rospy.Subscriber("fiducial_transforms", FiducialTransformArray, self.fiducial_cb)
         
         # Añadimos el publicador
-        self.position_pub = rospy.Publisher("aruco_position", Float32MultiArray, queue_size=10)
+        self.position_pub = rospy.Publisher("aruco_position", Pose, queue_size=10)
 
         self.aruco = FiducialTransformArray()
         self.needed_id = 721
@@ -32,30 +33,34 @@ class ArucoDetect():
         if len(self.aruco.transforms) >= 1:
             found_id = self.aruco.transforms[0].fiducial_id
 
-            if found_id == self.needed_id:
-                self.pos_x = round(self.aruco.transforms[0].transform.translation.x, 5)
-                self.pos_y = round(self.aruco.transforms[0].transform.translation.y, 5)
-                self.pos_z = round(self.aruco.transforms[0].transform.translation.z, 5)
+            #if found_id == self.needed_id:
+            self.pos_x = round(self.aruco.transforms[0].transform.translation.x, 5)
+            self.pos_y = round(self.aruco.transforms[0].transform.translation.y, 5)
+            self.pos_z = round(self.aruco.transforms[0].transform.translation.z, 5)
 
-                quat = [round(self.aruco.transforms[0].transform.rotation.x, 5),
-                        round(self.aruco.transforms[0].transform.rotation.y, 5),
-                        round(self.aruco.transforms[0].transform.rotation.z, 5),
-                        round(self.aruco.transforms[0].transform.rotation.w, 5)]
+            quat = [round(self.aruco.transforms[0].transform.rotation.x, 5),
+                    round(self.aruco.transforms[0].transform.rotation.y, 5),
+                    round(self.aruco.transforms[0].transform.rotation.z, 5),
+                    round(self.aruco.transforms[0].transform.rotation.w, 5)]
 
-                euler = euler_from_quaternion(quat)
-                yaw = round(euler[2], 5)
+            euler = euler_from_quaternion(quat)
+            yaw = round(euler[2], 5)
 
-                print("\nAruco ID:", str(found_id))
-                print("\nPosition in x:", str(self.pos_x))
-                print("\nPosition in y:", str(self.pos_y))
-                print("\nPosition in z:", str(self.pos_z))
-                print("\nOrientation in yaw:", str(yaw))
+            print("\nAruco ID:", str(found_id))
+            print("\nPosition in x:", str(self.pos_x))
+            print("\nPosition in y:", str(self.pos_y))
+            print("\nPosition in z:", str(self.pos_z))
+            print("\nOrientation in yaw:", str(yaw))
 
-                # Publicar la posición como un Vector3
-                pos_vector = Float32MultiArray(self.pos_x, self.pos_y, self.pos_z)
-                self.position_pub.publish(pos_vector)
-            else:
-                print("\nNeeded ID not found")
+            # Publicar la posición como un Vector3
+            pos_vector = Pose()
+            pos_vector.position.x = self.pos_x
+            pos_vector.position.y = self.pos_y
+            pos_vector.position.z = self.pos_z
+            pos_vector.orientation.z = yaw
+            self.position_pub.publish(pos_vector)
+            #else:
+            #print("\nNeeded ID not found")
 
 
 if __name__ == "__main__":
